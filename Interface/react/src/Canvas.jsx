6867bad6d1604090c9ed415d3095a27e80ce8a29
Emmanuel Senft <esenft@wisc.edu>
2022-03-30 17:19:36 -0500

    Initial work interface

---
+++ Interface/react/src/Canvas.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import useAppStore from './AppStore';
+import { Stage, Layer, Circle, Shape, Text } from 'react-konva';
+
+export const Canvas = (props) => {
+    var [corners,path] = useAppStore(state=>[state.corners,state.path]);
+    const [maxWidth,maxHeight] = useAppStore(state=>[state.imageWidth, state.imageHeight])
+    const setCorner = useAppStore(state=>state.setCorner)
+
+    const handleDragEnd = (e) => {
+        setCorner(e.target.attrs["cornerId"],Math.min(Math.max(e.target.attrs["x"],0),maxWidth),Math.min(Math.max(e.target.attrs["y"],0),maxHeight))
+        corners.map((corner) => {
+          return {
+            ...corner,
+            isDragging: false,
+          };
+        }
+      );
+    };
+    const handleMove = (e) => {
+        e.target.attrs["x"]=Math.min(Math.max(e.target.attrs["x"],0),maxWidth)
+        e.target.attrs["y"]=Math.min(Math.max(e.target.attrs["y"],0),maxHeight)
+        setCorner(e.target.attrs["cornerId"],e.target.attrs["x"],e.target.attrs["y"])
+        corners.map((corner) => {
+          return {
+            ...corner
+          };
+        }
+      );
+    };
+    return ( 
+    <Stage width={maxWidth} height={maxHeight}>
+        <Layer>
+          <Shape
+            sceneFunc={(context, shape) => {
+              context.beginPath();
+              context.moveTo(path[0].x, path[0].y);
+              path.map(point=>{
+                context.lineTo(point.x, point.y);  
+              })
+              // (!) Konva specific method, it is very important
+              context.fillStrokeShape(shape);
+            }}
+            stroke="green"
+            opacity={.5}
+            strokeWidth={4}
+          />
+
+          <Shape
+            sceneFunc={(context, shape) => {
+              context.beginPath();
+              context.moveTo(corners[0].x, corners[0].y);
+              context.lineTo(corners[1].x, corners[1].y);
+              context.lineTo(corners[2].x, corners[2].y);
+              context.lineTo(corners[3].x, corners[3].y);
+              context.closePath();
+              // (!) Konva specific method, it is very important
+              context.fillStrokeShape(shape);
+            }}
+            fill="#00D2FF"
+            stroke="red"
+            opacity={.1}
+            strokeWidth={4}
+          />
+          {corners.map((corner) => (
+            <Circle
+                key={corner.id}
+                id={corner.id}
+                cornerId={corner.id}
+                x={corner.x}
+                y={corner.y}
+                Radius={20}
+                fill="#0000ff"
+                opacity={0.8}
+                draggable
+                shadowColor="black"
+                shadowBlur={10}
+                shadowOpacity={0.6}
+                shadowOffsetX={corner.isDragging ? 10 : 5}
+                shadowOffsetY={corner.isDragging ? 10 : 5}
+                scaleX={corner.isDragging ? 1.2 : 1}
+                scaleY={corner.isDragging ? 1.2 : 1}
+                // onDragStart={handleDragStart}
+                onDragMove={handleMove}
+                onDragEnd={handleDragEnd} />
+          ))
+          }
+        </Layer>
+      </Stage>
+    )
+}
