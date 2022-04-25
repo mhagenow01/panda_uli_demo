f25286caaf135a8518f65447ed27dfa174b59000
mike <mhagenow@wisc.edu>
2022-04-25 07:51:27 -0500

    create a RT filter class

--- Execution/core_robotics/nodes/core_robotics/filters.py
+++ Execution/core_robotics/nodes/core_robotics/filters.py
@@ -6,6 +6,45 @@
 __author__ = "Guru Subramani and Mike Hagenow"
 
 from scipy.signal import butter,filtfilt
+import numpy as np
+
+class ButterLPFilter:
+    def __init__(self,order,wn,fs,num_vars):
+        self.order = order
+        self.wn = wn
+        self.fs = fs
+        self.num_vars = num_vars
+        self.b, self.a = butter(self.order,self.wn,fs=self.fs)
+
+        print(len(self.b), len(self.a))
+
+        self.coefflen = len(self.b)
+        self.xs = []
+        self.ys = []
+        for ii in range(0,self.coefflen):
+            self.xs.append(np.zeros((self.num_vars)))
+            self.ys.append(np.zeros((self.num_vars)))
+        
+    
+    def getFilteredOutput(self,signal):
+        if len(signal) != self.num_vars:
+            print("FILTERS.PY: trying to filter with wrong length of variables")
+            return signal
+        val = np.zeros((self.num_vars))
+        self.xs.insert(0,signal)
+        self.xs.pop(-1)
+        for ii in range(0,self.coefflen):
+            val += self.b[ii]*self.xs[ii]
+            if ii!=0:
+                val -= self.a[ii]*self.ys[ii-1]
+
+        val = val / self.a[0]
+        
+        self.ys.insert(0,val)
+        self.ys.pop(-1)
+    
+        return val
+
 
 
 def butter_lowpass(cutOff, fs, order=5):
