db350aca1e9f19e365157096626128dd3c45c61a
Mike Hagenow <mhagenow@wisc.edu>
2022-06-09 12:07:43 -0500

    add srv to collisionfreeik

--- Actuation/collision_free_ik/cpp_src/main.cpp
+++ Actuation/collision_free_ik/cpp_src/main.cpp
@@ -11,9 +11,12 @@
 #include <array>
 #include <vector>
 #include "franka_core_msgs/JointCommand.h"
+#include "collision_free_ik/CFIK.h"
 
 using namespace std;
 
+bool valid_ee;
+IKSolver* iksolver;
 
 string readFile(string fp) {
     
@@ -30,7 +33,6 @@ string readFile(string fp) {
     return allText;
 }
 
-
 void poseToArray(const geometry_msgs::Pose& msg, array<double, 7>& trans) {
     trans[0] = msg.position.x;
     trans[1] = msg.position.y;
@@ -42,13 +44,72 @@ void poseToArray(const geometry_msgs::Pose& msg, array<double, 7>& trans) {
     trans[6] = msg.orientation.z;
 }
 
+bool solveIKSrv(collision_free_ik::CFIK::Request &req,
+             collision_free_ik::CFIK::Response &res){
+
+        geometry_msgs::Pose desired_pose = req.desired_pose;
+        std_msgs::Float64MultiArray joints_in; req.starting_joints;
+
+        vector<double> current_q = vector<double>();
+        for (unsigned int j = 0; j < joints_in.data.size(); j++) {
+                current_q.push_back(joints_in.data[j]);
+        }
+
+        if (!valid_ee) {
+            ROS_WARN("No EE specified for IK.");
+            return false;
+        }
+
+        if (iksolver->dof() != 7) {
+            ROS_WARN("This only works if you are commanding 7 dof.");
+            return false;
+        }
+
+        array<double, 7> trans = array<double, 7>();
+        poseToArray(desired_pose, trans);
+
+        vector<double> q = vector<double>();
+        for (size_t i = 0; i < iksolver->dof(); i++) {
+            q.push_back(0);
+        }
+
+        if (iksolver->solve(current_q.data(), trans, q.data())) {
+            geometry_msgs::Pose pose_out;
+            
+            // Store joint angles in array message
+            std_msgs::Float64MultiArray joints_out;
+            joints_out.data.resize(q.size());
+            for (unsigned int j = 0; j < q.size(); j++) {
+                joints_out.data[j] = q[j];
+            }
+            res.soln_joints = joints_out;
+
+            // ES: TODO put in FK from IK
+            pose_out.orientation.x = 0.0;
+            pose_out.orientation.y = 0.0;
+            pose_out.orientation.z = 0.0;
+            pose_out.orientation.w = 1.0;
+            pose_out.position.x = 0.0;
+            pose_out.position.y = 0.0;
+            pose_out.position.z = 0.0;
+            res.soln_pose = pose_out;
+            
+            return true;
+            
+        } else {
+            ROS_WARN("Couldn't do some IK");
+            return false;
+        }
+}
+
+
 int main(int argc, char** argv) {
     ros::init(argc, argv, "collision_free_ik");
     ros::NodeHandle n;
 
     string urdf, ee_frame, arm_colliders, environment, solver_config;
     string urdf_fp, arm_collider_fp, environment_fp, solver_config_fp;
-    bool valid_ee = true;
+    valid_ee = true;
     vector<double> current_q;
     vector<string> joint_names;
     n.getParam("/default_end_effector_frame", ee_frame);
@@ -64,12 +125,14 @@ int main(int argc, char** argv) {
     environment = readFile(environment_fp);
     solver_config = readFile(solver_config_fp);
     
-    IKSolver iksolver = IKSolver(urdf, ee_frame, arm_colliders, environment, solver_config);
-    if (current_q.size() != iksolver.dof()) {
-        cout << "IK SOLVER NUM DOF: " << iksolver.dof() << endl;
+    iksolver = new IKSolver(urdf, ee_frame, arm_colliders, environment, solver_config);
+    if (current_q.size() != iksolver->dof()) {
+        cout << "IK SOLVER NUM DOF: " << iksolver->dof() << endl;
         cout << "for file: " << urdf_fp << endl;
         return 1;
     }
+
+    ros::ServiceServer service = n.advertiseService("/collision_free_ik/solve_ik", solveIKSrv);
     
     ros::Publisher pub = n.advertise<franka_core_msgs::JointCommand>("out", 1);
     array<double, 7> trans = array<double, 7>();
@@ -88,7 +151,7 @@ int main(int argc, char** argv) {
         string frame = msg->data;
 
         if (frame.compare(ee_frame) != 0) {
-            if (!iksolver.set_ee(frame.c_str())) {
+            if (!iksolver->set_ee(frame.c_str())) {
                 ROS_WARN("Failed to change end effector to frame.");
                 // Don't do anything if we don't know the frame.
                 valid_ee = false;
@@ -108,7 +171,7 @@ int main(int argc, char** argv) {
             return;
         }
 
-        if (iksolver.dof() != 7) {
+        if (iksolver->dof() != 7) {
             ROS_WARN("This only works if you are commanding 7 dof.");
             return;
         }
@@ -116,11 +179,11 @@ int main(int argc, char** argv) {
         poseToArray(pose, trans);
 
         vector<double> q = vector<double>();
-        for (size_t i = 0; i < iksolver.dof(); i++) {
+        for (size_t i = 0; i < iksolver->dof(); i++) {
             q.push_back(0);
         }
 
-        if (iksolver.solve(current_q.data(), trans, q.data())) {
+        if (iksolver->solve(current_q.data(), trans, q.data())) {
             auto out_msg = franka_core_msgs::JointCommand();
             out_msg.names = joint_names;
             out_msg.position = q;
