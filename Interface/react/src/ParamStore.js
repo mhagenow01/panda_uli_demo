ba436a6fc012eba7bb5189bc05f63164f536c93d
Emmanuel Senft <esenft@wisc.edu>
2022-07-14 15:31:54 -0500

    Adding param store

---
+++ Interface/react/src/ParamStore.js
@@ -0,0 +1,30 @@
+import create from "zustand";
+import produce from 'immer';
+import ROSLIB from 'roslib';
+
+import useRosStore from './RosStore';
+
+const immer = (config) => (set, get, api) =>
+  config((fn) => set(produce(fn)), get, api);
+
+const store = (set,get) => ({
+    parameters: [ {type: "select", id:"passes", label:"# of passes", value:"2", options:["2","3","4","5","6","7"]},
+                  {type: "select", id:"direction", label:"Orientation", value:"Horizontal", options:["Horizontal","Vertical"]},
+                  {type: "select", id:"material", label:"Material", value:"Composite", options:["Composite","Metal","Paint"]},
+                  {type: "slider", id:"force", label:"Force", value:7,min:0,max:20,unit: "N"},
+                  {type: "slider", id:"speed", label:"Feed Rate", value:10,min:1,max:20, unit: "mm/s"},
+                  {type: "slider", id:"pitch", label:"Pitch angle", value:0,min:-10,max:10, unit: "deg"},
+                  {type: "select", id:"tool", label:"Tool", value:"pandaOrbital", options:["pandaOrbital","panda_gripper"]}
+                ],
+    setParameter: (idx,value) => set(state=>{
+       state.parameters[idx].value = value
+       useRosStore.getState().paramTopic.publish({data:JSON.stringify(state.parameters)})
+     }),
+     publishStates: () => set(state=>{
+      useRosStore.getState().paramTopic.publish({data:JSON.stringify(state.parameters)})
+     })
+});
+
+const useParamStore = create(immer(store));
+
+export default useParamStore;
