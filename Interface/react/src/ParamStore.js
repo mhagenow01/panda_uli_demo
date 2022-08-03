3f3587c903586987ece734d7554584762b9bba01
Emmanuel Senft <esenft@wisc.edu>
2022-08-03 07:31:35 -0700

    Demo version

--- Interface/react/src/ParamStore.js
+++ Interface/react/src/ParamStore.js
@@ -12,9 +12,9 @@ const store = (set,get) => ({
     parameters: [ {type: "select", id:"passes", label:"# of passes", value:"2", options:["2","3","4","5","6","7"]},
                   {type: "select", id:"direction", label:"Orientation", value:"Horizontal", options:["Horizontal","Vertical"]},
                   {type: "select", id:"pattern", label:"Pattern", value:"None", options:["None","Circles"]},
-                  {type: "slider", id:"force", label:"Force", value:7,min:0,max:20,unit: "N"},
-                  {type: "slider", id:"speed", label:"Feed Rate", value:10,min:1,max:60, unit: "mm/s"},
-                  {type: "slider", id:"pitch", label:"Pitch angle", value:0,min:-10,max:10, unit: "deg"},
+                  {type: "slider", id:"force", label:"Force", value:14,min:0,max:30,unit: "N"},
+                  {type: "slider", id:"speed", label:"Feed Rate", value:20,min:1,max:100, unit: "mm/s"},
+                  {type: "slider", id:"pitch", label:"Pitch angle", value:0,min:-5,max:5, unit: "deg"},
                   {type: "select", id:"tool", label:"Tool", value:"pandaOrbital", options:["pandaOrbital","panda_gripper"]}
                 ],
     setParameter: (idx,value) => set(state=>{
